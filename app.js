247f892efa2b410650e4881970fb6084d8eaf5a4
Zhu-Peter <gzhu@ualberta.ca>
2024-04-03 18:52:16 -0600

    custom function

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-let usernames = ["Jake", "John", "Jason", "Jacob", "JoWinnernes"];
+let usernames = ["Jake", "John", "Jason", "Jacob", "Jones"];
 
 let counter = 0;
-let usernames_;
+// let usernames_;
+// while(counter < usernames.length){
+//     if (usernames[counter].toLowerCase().includes("winner")){
+//         console.log("we have a winner");
+//     }
+//     counter++;
+// }
+
+function specific_greeting(user) {
+    return "Hello and welcome "+user;
+}
+
 while(counter < usernames.length){
-    if (usernames[counter].toLowerCase().includes("winner")){
-        console.log("we have a winner");
-    }
-    counter++;
+    console.log(specific_greeting(usernames[counter]));
+    counter++;  
 }
